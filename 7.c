09085a074623cdaca8d70e49b62657a11fcd19dd
Anirudh890 <anirudhk@Anirudh.local>
2018-03-09 22:55:39 -0500

    First Commit

---
+++ 7.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,k,n;
+	
+	printf("Enter number : ");
+	scanf("%d",&n);
+	//ASCII Code for 'A' is 65
+	for(i=1;i<=n;i++)
+	{
+		//for(k=1;k<=(n-i);k++)
+			//{
+			 //printf(" ");
+			//}
+		for(j=1;j<=i;j++)
+			{
+			 printf("%c",64+i);
+			}
+		for(j=2;j<=i;j++)
+			{
+			 printf("%c",64+i);
+			}
+ 
+		printf("\n");
+	}
+	return 0;
+}
